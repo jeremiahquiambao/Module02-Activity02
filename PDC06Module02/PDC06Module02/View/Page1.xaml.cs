e4189d450a118695abd28405c959478f0ae0c2a1
User <User@B20>
2023-09-15 11:45:16 +0800

    Add project files.

---
+++ PDC06Module02/PDC06Module02/View/Page1.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PDC06Module02.ViewModel;
+
+namespace PDC06Module02.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PDC06Module02/PDC06Module02/ViewModel/StudentViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Text;
+using PDC06Module02.Model;
+using Xamarin.Forms;
+
+namespace PDC06Module02.ViewModel
+{
+    class StudentViewModel : INotifyPropertyChanged
+    {
+        public student StudentSet { get; set; }
+        public Command SubmitEntry { get; set; }
+
+
+        public string SubjectCodeEntry { get; set; }
+        public string SubjectCodeDisplay { get; set; }
+
+        public string SubjectTitleEntry { get; set; }
+        public string SubjectTitleDisplay { get; set; }
+
+        public string SubjectUnitEntry{ get; set; }
+        public string SubjectUnitDisplay { get; set; }
+
+        public StudentViewModel() 
+        {
+            SubmitEntry = new Command(() =>
+            {
+                StudentSet = new student 
+                {
+                    Subjectcode = SubjectCodeEntry,
+                };
+
+                SubjectCodeDisplay = SubjectCodeEntry;
+                SubjectTitleEntry = SubjectTitleEntry;
+                SubjectUnitDisplay = SubjectUnitEntry;
+
+                OnPropertyChanged(nameof(SubjectCodeDisplay));
+                OnPropertyChanged(nameof(SubjectCodeEntry));
+
+                OnPropertyChanged(nameof(SubjectTitleDisplay));
+                OnPropertyChanged(nameof(SubjectTitleEntry));
+
+                OnPropertyChanged(nameof(SubjectUnitDisplay));
+                OnPropertyChanged(nameof(SubjectUnitEntry));
+            });
+
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
